29a2ebdf6f7eb793322bdace8aa778f2c84c75ca
Umut Tunçer <utuncerx@gmail.com>
2025-03-16 22:07:34 +0300

    Kullanıcıdan Değer Alma, Tür Değiştirme, Matematik İşlemleri ve Matematiksel Fonksiyonlar Öğrenildi

---
+++ test/main.py
@@ -0,0 +1,55 @@
+"""
+#print("hello world")
+isim = "Muhammed" #string
+fiyat = 13.52 #float
+x = 3 #integer
+AldiMi = True # boolean
+"""
+
+"""
+kitapIsmi = "Moby Dick"
+kitapSayfasi = 195
+kitapAgirligi = 13.45
+kitapYeniMi = True
+print(type(kitapAgirligi)) #Değşkenin türünü öğrenmek için (<class 'float'>)
+"""
+
+#Kullanıcıdan değer alma işlemleri
+"""
+isim = input("İsminiz Nedir? ")
+print("Hoşgeldin " + isim)
+
+isim = input("Merhabalar adınız nedir? ")
+yemek = input("En sevdiğiniz yemek nedir? ")
+
+print(isim + " " + yemek + " sever")
+"""
+
+#Tür Değiştirme
+"""
+int()
+float()
+str()
+bool()
+"""
+
+#Matematik İşlemleri
+# x=9
+# y=5
+# print(x//y)#Bu işlem sayıları tam olarak böler . dan sonrak değeriyazpmaz örneğin 9/5 1.8 ken ekrana 1 yazar
+# print(x**y)#Bu işlem x üzeri y olarak üslü sayı işlemi yapmaya yarar
+"""
+sayi1 = input("1. Sayıyı girinz: ")
+sayi2 = input("2. Sayıyı girinz: ")
+print(int(sayi1) + int(sayi2))
+"""
+
+#Matematik Fonksiyonlar
+# print(round(9.5)) # yuvarlama işlemidir virgülden sonraki 5 ve 5'den büyük değerleri bir üst sayıya altındakileri bir alt sayıya yuvarlar çıktı = 10
+# print(abs(-9)) # Mutlak değer alma işlemidir
+#SAYİNİN karekökünü almak için dışarıdan kütüphane eklenmeli
+# import math
+# print(math.sqrt(9))
+# min(9,2,5) # En küçük değer
+# max(9,2,5) # En büyük değer
+
